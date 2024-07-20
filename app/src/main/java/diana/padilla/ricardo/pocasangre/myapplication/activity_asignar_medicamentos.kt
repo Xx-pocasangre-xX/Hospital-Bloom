baa9251a2332f5cc57a3b2d41fc10e9b83ef833c
Diana <20200005@ricaldone.edu.sv>
2024-07-20 15:54:20 -0600

    Terminé todas las interfaces

--- app/src/main/java/diana/padilla/ricardo/pocasangre/myapplication/activity_asignar_medicamentos.kt
+++ app/src/main/java/diana/padilla/ricardo/pocasangre/myapplication/activity_asignar_medicamentos.kt
@@ -3,35 +3,27 @@ package diana.padilla.ricardo.pocasangre.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class activity_registro_pacientes : AppCompatActivity() {
+class activity_asignar_medicamentos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_registro_pacientes)
+        setContentView(R.layout.activity_asignar_medicamentos)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnMedicamentos = findViewById<Button>(R.id.btnMedicamentos)
-        btnMedicamentos.setOnClickListener {
+        val btnRegresar = findViewById<Button>(R.id.btnRegresar)
+        btnRegresar.setOnClickListener {
             val pantallaNuevosMedicamentos = Intent(this, activity_nuevos_medicamentos::class.java)
             startActivity(pantallaNuevosMedicamentos)
-            overridePendingTransition(0, 0)
-        }
-
-        val btnHome = findViewById<ImageView>(R.id.btnHome)
-        btnHome.setOnClickListener {
-            val pantallaListadoPacientes = Intent(this, activity_listado_pacientes::class.java)
-            startActivity(pantallaListadoPacientes)
-            overridePendingTransition(0, 0)
+            overridePendingTransition(0,0)
         }
     }
 }
