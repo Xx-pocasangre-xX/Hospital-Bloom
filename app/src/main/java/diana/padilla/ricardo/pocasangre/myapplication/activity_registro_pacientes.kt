331968a48f80c2a6d176d802ad8c06e3f3696578
Diana <20200005@ricaldone.edu.sv>
2024-07-20 13:03:41 -0600

    Creación de pantalla de registro de pacientes

--- app/src/main/java/diana/padilla/ricardo/pocasangre/myapplication/activity_registro_pacientes.kt
+++ app/src/main/java/diana/padilla/ricardo/pocasangre/myapplication/activity_registro_pacientes.kt
@@ -1,28 +1,20 @@
 package diana.padilla.ricardo.pocasangre.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class activity_listado_pacientes : AppCompatActivity() {
+class activity_registro_pacientes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_listado_pacientes)
+        setContentView(R.layout.activity_registro_pacientes)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnAgregar = findViewById<ImageView>(R.id.btnAgregar)
-        btnAgregar.setOnClickListener {
-            val pantallaRegistroPacientes = Intent(this, activity_registro_pacientes::class.java)
-            startActivity(pantallaRegistroPacientes)
         }
     }
 }
