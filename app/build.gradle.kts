b437d2eb65479fc3dc49e8952a0e886691e70600
isjustSammy <20220631@ricaldone.edu.sv>
2024-07-20 10:12:23 -0600

    Termine pantalla de carga y bienvenida

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,11 @@ android {
 
 dependencies {
 
+    implementation("com.airbnb.android:lottie:6.4.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation("com.oracle.database.jdbc:ojdbc6:11.2.0.4")
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
