e56f03d9aec818632806566e8445b10d3d54e795
Diana <20200005@ricaldone.edu.sv>
2024-07-20 15:10:43 -0600

    Terminé pantalla de nuevos medicamentos

--- app/src/main/java/diana/padilla/ricardo/pocasangre/myapplication/activity_registro_pacientes.kt
+++ app/src/main/java/diana/padilla/ricardo/pocasangre/myapplication/activity_registro_pacientes.kt
@@ -1,6 +1,8 @@
 package diana.padilla.ricardo.pocasangre.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class activity_registro_pacientes : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnMedicamentos = findViewById<Button>(R.id.btnMedicamentos)
+        btnMedicamentos.setOnClickListener {
+            val pantallaNuevosMedicamentos = Intent(this, activity_nuevos_medicamentos::class.java)
+            startActivity(pantallaNuevosMedicamentos)
+            overridePendingTransition(0, 0)
         }
     }
 }
