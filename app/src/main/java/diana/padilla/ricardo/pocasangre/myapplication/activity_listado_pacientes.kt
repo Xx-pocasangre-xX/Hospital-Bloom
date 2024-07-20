d56fc420fff5d8e204821895b0dbaa77114bcd26
Diana <20200005@ricaldone.edu.sv>
2024-07-20 11:25:15 -0600

    Creación de pantalla de listado de pacientes

--- app/src/main/java/diana/padilla/ricardo/pocasangre/myapplication/activity_listado_pacientes.kt
+++ app/src/main/java/diana/padilla/ricardo/pocasangre/myapplication/activity_listado_pacientes.kt
@@ -1,28 +1,20 @@
 package diana.padilla.ricardo.pocasangre.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class activity_ingreso : AppCompatActivity() {
+class activity_listado_pacientes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ingreso)
+        setContentView(R.layout.activity_listado_pacientes)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnIniciarSesionIngreso = findViewById<Button>(R.id.btnIniciarSesionIngreso)
-        btnIniciarSesionIngreso.setOnClickListener {
-            val pantallaListadoPacientes = Intent(this, activity_listado_pacientes::class.java)
-            startActivity(pantallaListadoPacientes)
         }
     }
 }
