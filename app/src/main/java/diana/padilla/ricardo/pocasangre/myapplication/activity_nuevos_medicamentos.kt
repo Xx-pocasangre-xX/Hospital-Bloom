8fa042c9ec0154fbf9e93f14cbff654eaf131297
Diana <20200005@ricaldone.edu.sv>
2024-07-20 20:59:46 -0600

    Terminé area de registro de medicamentos

--- app/src/main/java/diana/padilla/ricardo/pocasangre/myapplication/activity_nuevos_medicamentos.kt
+++ app/src/main/java/diana/padilla/ricardo/pocasangre/myapplication/activity_nuevos_medicamentos.kt
@@ -1,12 +1,20 @@
 package diana.padilla.ricardo.pocasangre.myapplication
 
+import Modelo.ClaseConexion
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class activity_nuevos_medicamentos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +39,43 @@ class activity_nuevos_medicamentos : AppCompatActivity() {
             val pantallaAsignarMedicamentos = Intent(this, activity_asignar_medicamentos::class.java)
             startActivity(pantallaAsignarMedicamentos)
             overridePendingTransition(0, 0)
+        }
+
+        val txtNombreMedicamento = findViewById<EditText>(R.id.txtNombreMedicamento)
+        val txtDescripcionMedicamento = findViewById<EditText>(R.id.txtDescripcionMedicamento)
+        val btnAgregarMedicamento = findViewById<Button>(R.id.btnAgregarMedicamento)
+
+        btnAgregarMedicamento.setOnClickListener {
+
+            val nombre = txtNombreMedicamento.text.toString()
+            val descripcion = txtDescripcionMedicamento.text.toString()
+
+            if(nombre.isEmpty() || descripcion.isEmpty()){
+                Toast.makeText(
+                    this,
+                    "Error, para guardar el medicamento debes llenar todas las casillas.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }else {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val objConexion = ClaseConexion().cadenaConexion()
+                    val addMedicamento =
+                        objConexion?.prepareStatement("INSERT INTO Medicamentos (nombre_medicamento, descripcion) VALUES (?, ?)")!!
+                    addMedicamento.setString(1, txtNombreMedicamento.text.toString())
+                    addMedicamento.setString(2, txtDescripcionMedicamento.text.toString())
+                    addMedicamento.executeQuery()
+
+                    withContext(Dispatchers.Main) {
+                        AlertDialog.Builder(this@activity_nuevos_medicamentos)
+                            .setTitle("Registro de medicamento exitoso!")
+                            .setMessage("El medicamento se ha guardado exitosamente.")
+                            .setPositiveButton("Aceptar", null)
+                            .show()
+                        txtNombreMedicamento.setText("")
+                        txtDescripcionMedicamento.setText("")
+                    }
+                }
+            }
         }
     }
 }
